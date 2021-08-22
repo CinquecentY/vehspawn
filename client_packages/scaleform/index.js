575db991d854507035a97a3c63bddeeac62b00dc
CinquecentY <81559422+CinquecentY@users.noreply.github.com>
2021-08-22 21:55:29 +0100

    Add files via upload

--- client_packages/scaleform/index.js
+++ client_packages/scaleform/index.js
@@ -32,6 +32,52 @@ class BasicScaleform {
         mp.game.graphics.popScaleformMovieFunctionVoid();
     }
 
+    callFunctionReturn(functionName,type, ...args) {
+        mp.game.graphics.pushScaleformMovieFunction(this.handle, functionName);
+
+        args.forEach(arg => {
+            switch(typeof arg) {
+                case "string": {
+                    mp.game.graphics.pushScaleformMovieFunctionParameterString(arg);
+                    break;
+                }
+
+                case "boolean": {
+                    mp.game.graphics.pushScaleformMovieFunctionParameterBool(arg);
+                    break;
+                }
+
+                case "number": {
+                    if(Number(arg) === arg && arg % 1 !== 0) {
+                        mp.game.graphics.pushScaleformMovieFunctionParameterFloat(arg);
+                    } else {
+                        mp.game.graphics.pushScaleformMovieFunctionParameterInt(arg);
+                    }
+                }
+            }
+        });
+        let methodReturn = mp.game.graphics.popScaleformMovieFunction();
+        //while(!mp.game.invoke('0x768FF8961BA904D6',methodReturn)) mp.game.wait(0);
+        switch (type) {
+            case "string":
+                return mp.game.graphics.sittingTv(methodReturn);
+            case "boolean":
+                return mp.game.invoke('0xD80A80346A45D761',methodReturn);
+            case "number":
+                return mp.game.invoke('0x2DE7EFA66B906036',methodReturn);
+            default:
+                return "nonce";
+        }
+            /*if(mp.game.graphics.sittingTv(methodReturn) != "")
+                return mp.game.graphics.sittingTv(methodReturn);
+            if(typeof mp.game.invoke('0xD80A80346A45D761',methodReturn) == "boolean")
+                return mp.game.invoke('0xD80A80346A45D761',methodReturn);
+            if(typeof mp.game.invoke('0x2DE7EFA66B906036',methodReturn) == "number")
+                return mp.game.invoke('0x2DE7EFA66B906036',methodReturn);
+            else
+                return "nonce";*/
+    }
+
     renderFullscreen() {
         mp.game.graphics.drawScaleformMovieFullscreen(this.handle, 255, 255, 255, 255, false);
     }
--- client_packages/vehspawn/index.js
+++ client_packages/vehspawn/index.js
@@ -3,49 +3,74 @@ const ScaleForm = require('scaleform');
 
 const vehicles = require('vehspawn/vehicleHashes');
 var vehicleClassList = setVehicleClassList();
+var vehicleList = setVehicleList(1);
 mp.events.addCommand("vehspawn",()=>{
     let scaleform = new ScaleForm('vehicle_spawn');
     scaleform.callFunction('SET_PLAYER_NAME',mp.players.local.name);
-    for (let i = 0; i < vehicleClassList.length; i++) {
-      scaleform.callFunction('ADD_VEH_CLASS',vehicleClassList[i][0],vehicleClassList[i][1]);
-    }
+    addVehicleClass(scaleform);
+    addVehicle(scaleform);
     mp.events.add('render', () => {
       if(scaleform)
       {
         /*mp.game.controls.disableAllControlActions(0);
-        mp.game.controls.setInputExclusive(28, 239);
-        mp.game.controls.setInputExclusive(28, 240);*/
-        scaleform.callFunction('SET_MOUSE_INPUT',mp.game.controls.getControlNormal(28, 239),mp.game.controls.getControlNormal(28, 240));
+        mp.game.controls.setInputExclusive(2, 239);
+        mp.game.controls.setInputExclusive(2, 240);*/
+        scaleform.callFunction('SET_MOUSE_INPUT',mp.game.controls.getControlNormal(2, 239),mp.game.controls.getControlNormal(2, 240));
         scaleform.renderFullscreen();
         if(mp.game.controls.isControlPressed(0,51))
         {
           scaleform.dispose();
           scaleform = null;
         }
-        if(mp.game.controls.isControlPressed(28,237))
+        if(mp.game.controls.isControlJustPressed(2,237))
         {
           scaleform.callFunction('SET_INPUT_EVENT',237);
+          let val = scaleform.callFunctionReturn('GET_CURRENT_SELECTION',"number");
+          mp.gui.chat.push(val.toString());
         }
-        if(mp.game.controls.isControlPressed(28,241))
+        if(mp.game.controls.isControlPressed(2,241))
         {
-          scaleform.callFunction('SET_SCROLL_INPUT',mp.game.controls.getControlNormal(28,241))
+          scaleform.callFunction('SET_SCROLL_INPUT',mp.game.controls.getControlNormal(2,241))
         }
-        if(mp.game.controls.isControlPressed(28,242))
+        if(mp.game.controls.isControlPressed(2,242))
         {
-          scaleform.callFunction('SET_SCROLL_INPUT',mp.game.controls.getControlNormal(28,242))
+          scaleform.callFunction('SET_SCROLL_INPUT',mp.game.controls.getControlNormal(2,242))
         }
         
       }  
     })
   });
 
+function addVehicle(scaleform) {
+  for (let i = 0; i < vehicleList.length; i++) {
+    scaleform.callFunction('ADD_VEHICLE', vehicleList[i][0], vehicleList[i][1]);
+  }
+}
+
+function addVehicleClass(scaleform) {
+  for (let i = 0; i < vehicleClassList.length; i++) {
+    scaleform.callFunction('ADD_VEH_CLASS', vehicleClassList[i][0], vehicleClassList[i][1]);
+  }
+}
+
   function setVehicleClassList()
   {
     var array=[];
     for (let i = 0; i < 23; i++)
     {
-      let vehclass = `VEH_CLASS_${i}`;
-      array[i] =[vehclass,mp.game.ui.getLabelText(vehclass)];
+      array.push([i,mp.game.ui.getLabelText(`VEH_CLASS_${i}`)]);
+    }
+    return array;
+  }
+  function setVehicleList(vehclass)
+  {
+    var array = [];
+    for (let i in vehicles){
+      if(mp.game.vehicle.getVehicleClassFromName(vehicles[i]) == vehclass)
+      {
+        let displayName = mp.game.vehicle.getDisplayNameFromVehicleModel(vehicles[i]);
+        array.push([vehicles[i],mp.game.ui.getLabelText(displayName)]);
+      }
     }
     return array;
   }
