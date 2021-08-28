a841c278bae66ec3a1aeec5824ba074749b8d5bb
CinquecentY <81559422+CinquecentY@users.noreply.github.com>
2021-08-28 12:03:03 +0100

    Update index.js

--- packages/vehspawn/index.js
+++ packages/vehspawn/index.js
@@ -1,4 +1,4 @@
 mp.events.add("vehspawn_Spawn", (player, model) => {
     var vehicle = mp.vehicles.new(model, player.position,{heading:player.heading});
-    //player.putIntoVehicle(vehicle, 0);
+    player.putIntoVehicle(vehicle, 0);
 });
